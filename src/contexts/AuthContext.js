449915f6832a928b3bacdfdca759778b642d5e11
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-03-11 23:07:11 +0100

    added auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
